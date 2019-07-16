e365b6c46ec147749394dc60e221f667964ac7d0
Tyler <tmoorewebdesigns@gmail.com>
2019-07-16 12:19:26 -0400

    Update social-icons.js

--- src/constants/social-icons.js
+++ src/constants/social-icons.js
@@ -1,5 +1,5 @@
 import React from "react"
-import { FaFacebook, FaTwitterSquare, FaSquarespace } from "react-icons/fa"
+import { FaFacebook, FaTwitterSquare, FaInstagram } from "react-icons/fa"
 
 export default [
   {
@@ -7,11 +7,11 @@ export default [
     url: "https://twitter.com",
   },
   {
-    icon: <FaTwitterSquare />,
+    icon: <FaInstagram />,
     url: "https://twitter.com",
   },
   {
-    icon: <FaSquarespace />,
+    icon: <FaTwitterSquare />,
     url: "https://twitter.com",
   },
 ]
