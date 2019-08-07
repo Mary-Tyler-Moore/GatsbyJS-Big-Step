c2537c15802bafec795bbb8562a0a6148c1c2406
Tyler <tmoorewebdesigns@gmail.com>
2019-08-07 15:27:49 -0400

    ...

--- src/components/Home/Staff.js
+++ src/components/Home/Staff.js
@@ -99,13 +99,13 @@ export default function Gallery() {
                 <Img fluid={img2} />
                 <p className="info-a">Stuart Gregory</p>
                 <p className="info-b">Co-Founder/COO/Admissions</p>
-                <p className="info-b">some text</p>
+                <p className="info-c">some text</p>
               </div>
               <div className="item item-3">
                 <Img fluid={img3} />
                 <p className="info-a">Brandon O'Mahoney</p>
                 <p className="info-b">Co-Founder/CFO</p>
-                <p className="info-b">some text</p>
+                <p className="info-c">some text</p>
               </div>
               <div className="item item-4">
                 <Img fluid={img4} />
@@ -117,7 +117,7 @@ export default function Gallery() {
                 <Img fluid={img5} />
                 <p className="info-a">Dr. Charlie Dean, MD</p>
                 <p className="info-b">Medical Director/Physician</p>
-                <p className="info-b">some text</p>
+                <p className="info-c">some text</p>
               </div>
               <div className="item item-6">
                 <Img fluid={img6} />
@@ -170,6 +170,12 @@ const GalleryWrapper = styled.div`
     text-align: center;
     line-height: 1rem;
     font-size: 1rem;
+  }
+  .info-c {
+    text-align: center;
+    line-height: 1rem;
+    font-size: 1rem;
+    color: #fff;
   }
   .info {
     position: absolute;
